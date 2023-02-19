ac38ba56f314e4dccf370fbe71a6948fa55d1762
Administrator <Administrator@PLABDC01>
2023-02-19 12:40:25 -0800

    Add project files.

---
+++ WK8Lab2___Recursive_Function_Fibonacci.py
@@ -0,0 +1,25 @@
+#Sean Holbrook
+#CIS261
+#Wk8Lab2 - Recursive Function Fibonacci
+
+
+
+#recursive function to return fibonacci for 16
+
+def fibonacci(num):
+    total = 1
+    if num <= 1:
+        return num
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+    
+
+#turn the output into a string using the recursive function
+sequence = ", ".join(str(fibonacci(n)) for n in range(16))
+
+#print the output of the fibonacci up to 16
+print("The Fibonacci series for 16")
+print(sequence)
+
+
+
